44ae59ab97afda728cd098b1f92a0456f13bdcde
SeraphyBR <luisjuniorbr@gmail.com>
2020-05-16 16:06:38 -0300

    Adicionado traveling_salesman_genetic.cpp

---
+++ src/traveling_salesman_genetic.cpp
@@ -0,0 +1,33 @@
+#include "../lib/graph.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace graph;
+
+int main(){
+    int num_cities {};
+
+    cin >> num_cities;
+    vector<pair<int,int>> cities(num_cities);
+
+    for(auto &city : cities){
+        cin >> city.first;
+        cin >> city.second;
+    }
+
+    Graph *g = new Graph(num_cities);
+    for(int i = 1; i <= num_cities; ++i){
+        auto cityA = cities.at(i);
+        for(int j = 1; j <= num_cities; ++j){
+            if(i != j){
+                auto cityB = cities.at(j);
+                auto xs = pow(cityB.first - cityA.first,2);
+                auto ys = pow(cityB.second - cityA.second,2);
+                double distance = sqrt(xs + ys);
+                g->add_conection(i, j, distance);
+            }
+        }
+    }
+
+    return 0;
+}
