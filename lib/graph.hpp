dd919034e1c296d325e201e4f1548fdfabd0c4e8
SeraphyBR <luisjuniorbr@gmail.com>
2020-05-16 16:05:15 -0300

    Update graph

--- lib/graph.hpp
+++ lib/graph.hpp
@@ -1,19 +1,21 @@
 #include <list>
 #include <vector>
 #include <cmath>
-using namespace std;
+using std::vector;
 
-class Graph {
-    private:
-        vector<vector<int>> *matrix_adj;
-        int vertices;
-        int default_value = -1;
-    public:
-        Graph(int vertices);
-        ~Graph();
-        void display();
-        int get_vertices();
-        int get_conection(int v1, int v2);
-        void add_conection(int v1, int v2, int value);
-        void del_conection(int v1, int v2);
-};
+namespace graph {
+    class Graph {
+        private:
+            vector<vector<double>> *matrix_adj;
+            int vertices;
+            double default_value = -1;
+        public:
+            Graph(int vertices);
+            ~Graph();
+            void display();
+            int get_vertices();
+            int get_conection(int v1, int v2);
+            void add_conection(int v1, int v2, double value);
+            void del_conection(int v1, int v2);
+    };
+}
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,39 +1,45 @@
 #include "../lib/graph.hpp"
 #include <iostream>
 
-Graph::Graph(int vertices){
-    this->vertices = vertices;
-    this->matrix_adj = new vector<vector<int>>(vertices,vector<int>(vertices, this->default_value));
-}
+using std::cout;
+using std::endl;
 
-Graph::~Graph(){
-    delete matrix_adj;
-    matrix_adj = nullptr;
-}
+namespace graph {
 
-void Graph::display(){
-    for (auto& line : *matrix_adj){
-        for (auto v : line){
-            cout << v << " ";
+    Graph::Graph(int vertices){
+        this->vertices = vertices;
+        this->matrix_adj = new vector<vector<double>>(vertices,vector<double>(vertices, this->default_value));
+    }
+
+    Graph::~Graph(){
+        delete this->matrix_adj;
+        this->matrix_adj = nullptr;
+    }
+
+    void Graph::display(){
+        for (auto& line : *matrix_adj){
+            for (auto v : line){
+                cout << v << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
-}
 
-void Graph::add_conection(int v1, int v2, int value){
-    if(v1 < this->vertices && v2 < this->vertices){
-        this->matrix_adj->at(v1).at(v2) = value;
-        this->matrix_adj->at(v2).at(v1) = value;
+    void Graph::add_conection(int v1, int v2, double value){
+        if(v1 < this->vertices && v2 < this->vertices){
+            this->matrix_adj->at(v1).at(v2) = value;
+            this->matrix_adj->at(v2).at(v1) = value;
+        }
     }
-}
 
-void Graph::del_conection(int v1, int v2){
-    if (v1 < this->vertices && v2 < this->vertices){
-        this->matrix_adj->at(v1).at(v2) = this->default_value;
-        this->matrix_adj->at(v2).at(v1) = this->default_value;
+    void Graph::del_conection(int v1, int v2){
+        if (v1 < this->vertices && v2 < this->vertices){
+            this->matrix_adj->at(v1).at(v2) = this->default_value;
+            this->matrix_adj->at(v2).at(v1) = this->default_value;
+        }
     }
-}
 
-int Graph::get_conection(int v1, int v2){
-    return this->matrix_adj->at(v1).at(v2);
+    int Graph::get_conection(int v1, int v2){
+        return this->matrix_adj->at(v1).at(v2);
+    }
 }
